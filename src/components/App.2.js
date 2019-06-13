0c9b3a6679315455c4f88f65305fbf7e02effd94
baiNan-git <692233099@qq.com>
2019-06-13 16:30:19 +0800

    构造器解决this指向

--- src/components/App.2.js
+++ src/components/App.2.js
@@ -3,16 +3,15 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor (props) {
     super(props);
-    this.btnClickFun = this.btnClick.bind(this)
     console.log('1', this);
   }
   btnClick () {
-    console.log('2', this); 
+    console.log('2', this);
   }
   render () {
     return (
       <div>
-        <button onClick = { this.btnClickFun }>react按钮</button>
+        <button onClick = { this.btnClick }>react按钮</button>
       </div>
     )
   }
