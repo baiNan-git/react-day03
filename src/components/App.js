58ddaca9e1456117311e27a5969af5936bc99424
baiNan-git <692233099@qq.com>
2019-06-13 16:26:37 +0800

    事件处理this

--- src/components/App.js
+++ src/components/App.js
@@ -3,34 +3,16 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor (props) {
     super(props);
-    this.state = {
-      list: [
-        {
-          brand: '康师傅',
-          data: ['老谭酸菜', '红烧牛肉', '鲜虾鱼板面', '小鸡炖蘑菇']
-        },
-        {
-          brand: '统一',
-          data: ['藤椒牛肉', '葱爆牛肉', '葱烧排骨面']
-        }
-      ]
-    }
+    console.log('1', this);
   }
-
+  btnClick () {
+    console.log('2', this);
+  }
   render () {
-    let arr = [];
-    this.state.list.map((item, index) => {
-      let arrItem = [];
-      item.data.map((ite, ind) => {
-        arrItem.push(<li key = { ind }>{ ite }</li>)
-      })
-      arr.push(<li key = { index }>{ item.brand } <ul>{ arrItem }</ul></li>)
-    })
-
     return (
-      <ul>
-        { arr }
-      </ul>
+      <div>
+        <button onClick = { this.btnClick }>react按钮</button>
+      </div>
     )
   }
 }
