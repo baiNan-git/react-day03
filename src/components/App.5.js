4e51997cdab59f19ee88653414da461b448f662a
baiNan-git <692233099@qq.com>
2019-06-13 16:57:06 +0800

    事件传参

--- src/components/App.5.js
+++ src/components/App.5.js
@@ -5,14 +5,15 @@ class App extends Component {
     super(props);
     console.log('1', this);
   }
-  clickFn (id) {
+  clickFn (event, id) {
     console.log(id, this)
   }
   render () {
     let _this = this;
     return (
       <div>
-        <button onClick = { this.clickFn.bind(this, 33) }>react按钮</button>
+        <button onClick = { function (e) {return _this.clickFn(e, 11)} }>react按钮</button>
+        <button onClick = { (e) => {_this.clickFn(e, 22)}}>react按钮2</button>
       </div>
     )
   }
